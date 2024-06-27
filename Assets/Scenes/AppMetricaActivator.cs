57b42bff1326934739e25915a8d9d33fb44b33c1
4116VyachelavPyatetsky <115720162+4116VyachelavPyatetsky@users.noreply.github.com>
2024-06-27 10:57:01 +0300

    Apk-comm

---
+++ Assets/Scenes/AppMetricaActivator.cs
@@ -0,0 +1,22 @@
+using Io.AppMetrica;
+using UnityEngine;
+
+public static class AppMetricaActivator
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Activate()
+    {
+        AppMetrica.Activate(new AppMetricaConfig("d1b94fe6-6972-4af2-a98a-fab939213b1a")
+        {
+            FirstActivationAsUpdate = !IsFirstLaunch(),
+        });
+    }
+
+    private static bool IsFirstLaunch()
+    {
+        // Implement logic to detect whether the app is opening for the first time.
+        // For example, you can check for files (settings, databases, and so on),
+        // which the app creates on its first launch.
+        return true;
+    }
+}
